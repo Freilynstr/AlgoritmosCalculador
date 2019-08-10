e32c627df1b5ccee39586550f087a709b8aa7386
Freilyn S. Tavárez <freilynstr@hotmail.es>
2019-08-10 08:26:49 -0400

    Las operaciones con pares están completas

--- CalculadoraFinal/CalculadoraFinal/Form1.Designer.cs
+++ CalculadoraFinal/CalculadoraFinal/Form1.Designer.cs
@@ -40,10 +40,9 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(9, 22);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(12, 27);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(298, 20);
+            this.label1.Size = new System.Drawing.Size(368, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "Inserte los valores separados por comas:";
             // 
@@ -51,20 +50,19 @@
             // 
             this.textBox1.AcceptsReturn = true;
             this.textBox1.AcceptsTab = true;
-            this.textBox1.Location = new System.Drawing.Point(9, 45);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Location = new System.Drawing.Point(12, 55);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(420, 82);
+            this.textBox1.Size = new System.Drawing.Size(559, 100);
             this.textBox1.TabIndex = 1;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(358, 131);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(477, 161);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(70, 32);
+            this.button1.Size = new System.Drawing.Size(93, 39);
             this.button1.TabIndex = 2;
             this.button1.Text = "Calcular";
             this.button1.UseVisualStyleBackColor = true;
@@ -74,10 +72,9 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(9, 158);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(12, 194);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(94, 20);
+            this.label2.Size = new System.Drawing.Size(115, 25);
             this.label2.TabIndex = 4;
             this.label2.Text = "Resultados:";
             // 
@@ -85,36 +82,36 @@
             // 
             this.textBox2.AcceptsReturn = true;
             this.textBox2.AcceptsTab = true;
-            this.textBox2.Location = new System.Drawing.Point(9, 180);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox2.Location = new System.Drawing.Point(12, 222);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(420, 102);
+            this.textBox2.Size = new System.Drawing.Size(559, 125);
             this.textBox2.TabIndex = 5;
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(358, 287);
-            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Location = new System.Drawing.Point(477, 353);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(70, 32);
+            this.button2.Size = new System.Drawing.Size(93, 39);
             this.button2.TabIndex = 6;
             this.button2.Text = "Limpiar";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(440, 327);
+            this.ClientSize = new System.Drawing.Size(587, 402);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Calculadora";
--- CalculadoraFinal/CalculadoraFinal/Form1.cs
+++ CalculadoraFinal/CalculadoraFinal/Form1.cs
@@ -15,28 +15,149 @@ namespace CalculadoraFinal
         public Form1()
         {
             InitializeComponent();
+            textBox1.KeyPress += TextBox1_KeyPress; 
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        // Handle the KeyPress event to print the type of character entered into the control.
+        private void TextBox1_KeyPress(object sender, KeyPressEventArgs e)
         {
+            string aceptados = "1234567890,." + Convert.ToChar(8);
 
+            if (aceptados.Contains("" + e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string[] numbers = textBox1.Text.Split(',');
-
-            textBox2.Text = "Los números son: ";
-
-            for (int i = 0; i < numbers.Length; i++)
+            string[] conjunto = textBox1.Text.Split(',');
+            int[] conjuntoNumerico = new int[conjunto.Length];
+            if(textBox1.Text != "")
             {
-                textBox2.Text += numbers[i]+" ";
+                for (int i = 0; i < conjunto.Length; i++)
+                {
+                    try
+                    {
+                        conjuntoNumerico[i] = Int32.Parse(conjunto[i]);
+                    }
+                    catch (Exception)
+                    {
+                        textBox2.Text = "El conjunto numérico no puede ser procesado";
+                    }
+                    
+                }
+                textBox2.Text = "";
+                ProcesarConjunto(conjuntoNumerico);
+            }
+            else
+            {
+                textBox2.Text = "El conjunto numérico no puede ser procesado";
             }
         }
 
-        private void textBox2_TextChanged(object sender, EventArgs e)
+        private void Button2_Click(object sender, EventArgs e)
         {
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
 
+        private void ProcesarConjunto(int[] conjunto)
+        {
+            if (conjunto.Length % 4 == 0)
+            {
+                //Se procesará en cuartetos
+                //return "Cuartetos";
+            }
+            else if (conjunto.Length % 3 == 0)
+            {
+                //Se procesará en ternas
+                int[][] ternasDelConjunto = new int[(conjunto.Length / 3)][];
+                int contador = 0;
+                for (int i = 0; i < conjunto.Length / 3; i++)
+                {
+                    ternasDelConjunto[i] = new int[2];
+                    for (int j = 0; j < 3; j++)
+                    {
+                        //Se obtienen los pares
+                        ternasDelConjunto[i][j] = conjunto[contador];
+                        contador++;
+                    }
+                }
+
+                realizarOperacionConTernas(ternasDelConjunto);
+            }
+            else if (conjunto.Length % 2 == 0)
+            {
+                //Se procesará en pares
+                int[][] paresDelConjunto = new int[(conjunto.Length / 2)][];
+                int contador = 0;
+                for (int i = 0; i < conjunto.Length / 2; i++)
+                {
+                    paresDelConjunto[i] = new int[2];
+                    for (int j = 0; j < 2; j++)
+                    {
+                        //Se obtienen los pares
+                        paresDelConjunto[i][j] = conjunto[contador];
+                        contador++;
+                    }
+                }
+
+                realizarOperacionConPares(paresDelConjunto);
+            }
+        }
+
+        private void realizarOperacionConPares(int[][] paresDelConjunto)
+        {
+            string figura;
+            for (int i = 0; i < paresDelConjunto.Length; i++)
+            {
+                if(paresDelConjunto[i][0] == paresDelConjunto[i][1])
+                {
+                    figura = "Esfera";
+                    double radio = paresDelConjunto[i][0];
+                    double perimetro =   2 * Math.PI * radio;
+                    double area = 4 * Math.PI * Math.Pow(radio, 2);
+                    textBox2.Text += "La figura es una " + figura + " y su perímetro es " + perimetro + " y su área es " + area + "\n";
+                }
+                else
+                {
+                    figura = "Rombo";
+                    double lado = paresDelConjunto[i][0];
+                    double altura = paresDelConjunto[i][1];
+                    double perimetro = 2 * (Math.Sqrt((Math.Pow(lado, 2)) + (Math.Pow(altura, 2))));
+                    double area = (lado * altura) / 2;
+                    textBox2.Text += "La figura es una " + figura + " y su perímetro es " + perimetro + " y su área es " + area + "\n";
+                }
+            }
+        }
+
+        private void realizarOperacionConTernas(int[][] ternasDelConjunto)
+        {
+            string figura;
+            for (int i = 0; i < ternasDelConjunto.Length; i++)
+            {
+                if ((ternasDelConjunto[i][0] == ternasDelConjunto[i][1]) && (ternasDelConjunto[i][1] == ternasDelConjunto[i][2]))
+                {
+                    figura = "Cubo";
+                    double lado = ternasDelConjunto[i][0];
+                    double perimetro = (4 * lado) + (4 * lado) + (4 * lado);
+                    double area = 0;
+                    textBox2.Text += "La figura es un " + figura + "su perímetro es " + perimetro + " y su área es " + area + "\n";
+                }
+                else if((ternasDelConjunto[i][0] == ternasDelConjunto[i][1]) || (ternasDelConjunto[i][0] == ternasDelConjunto[i][2]))
+                {
+                    //double lado = paresDelConjunto[i][0];
+                    //double altura = paresDelConjunto[i][1];
+                    figura = "Pirámide";
+                    double resultado = 0;//(lado * altura) / 2;
+                    textBox2.Text += "La figura es una" + figura + " y el resultado es " + resultado + "\n";
+                }
+            }
         }
     }
 }
